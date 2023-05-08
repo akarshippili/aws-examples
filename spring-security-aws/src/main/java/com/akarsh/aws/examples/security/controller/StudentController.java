f304a7c9d0eef6690d895613fe4ee7dee01a548b
IPPILIAKARSH <ippiliakarsh@gmail.com>
2023-05-09 03:14:05 +0530

    Update StudentController.java

--- spring-security-aws/src/main/java/com/akarsh/aws/examples/security/controller/StudentController.java
+++ spring-security-aws/src/main/java/com/akarsh/aws/examples/security/controller/StudentController.java
@@ -2,25 +2,29 @@ package com.akarsh.aws.examples.security.controller;
 
 import com.akarsh.aws.examples.security.dto.StudentDto;
 import com.akarsh.aws.examples.security.service.StudentService;
+import lombok.extern.slf4j.Slf4j;
 import org.hibernate.dialect.LobMergeStrategy;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class StudentController {
 
-    private StudentService studentService;
+    private final StudentService studentService;
 
     public StudentController(StudentService studentService) {
         this.studentService = studentService;
     }
 
     @GetMapping(path = "/students/{studentId}")
-    public ResponseEntity<StudentDto> getStudentById(@PathVariable Long studentId) {
+    public ResponseEntity<StudentDto> getStudentById(Authentication authentication, @PathVariable Long studentId) {
+        log.info("getStudentById Resource accessed by {}", authentication.getName());
         return new ResponseEntity<>(studentService.getStudentById(studentId), HttpStatus.OK);
     }
 
